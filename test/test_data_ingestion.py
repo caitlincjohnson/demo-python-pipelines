bb075ec08139f55d6e9e9ea8886ce3866fed2986
Caitlin Johnson <35669839+caitlincjohnson@users.noreply.github.com>
2023-06-08 17:34:57 -0600

    Create test_data_ingestion.py

---
+++ test/test_data_ingestion.py
@@ -0,0 +1,10 @@
+import unittest
+from data_ingestion import fetch_data_from_api
+
+class TestDataIngestion(unittest.TestCase):
+    def test_fetch_data_from_api(self):
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
